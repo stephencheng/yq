4dde195e678ecd5b51f61a7c840392e3f2a78e76
stephen cheng <stephencheng@nbnco.com.au>
2020-03-11 00:22:48 +1100

    add test cases for reading yml

--- cmd/upsvc.go
+++ cmd/upsvc.go
@@ -12,19 +12,6 @@ func setLogLevel(level string) {
 	var backend = logging.AddModuleLevel(
 		logging.NewBackendFormatter(logging.NewLogBackend(os.Stderr, "", 0), format))
 
-	//CRITICAL
-	//ERROR v
-	//WARNING vv
-	//NOTICE vvv
-	//INFO vvvv
-	//DEBUG vvvvv
-
-	//if verbose {
-	//	backend.SetLevel(logging.DEBUG, "")
-	//} else {
-	//	backend.SetLevel(logging.ERROR, "")
-	//}
-
 	logLevel := logging.CRITICAL
 
 	switch level {
@@ -45,73 +32,22 @@ func setLogLevel(level string) {
 
 }
 
-func UpReadYml(logLevel string) error {
-	var path = "tasks.**.task"
-
+func UpReadYml(ymlstr, path, logLevel string) (string, error) {
 	setLogLevel(logLevel)
 	var updateAll, docIndexInt, errorParsingDocIndex = parseDocumentIndex()
 	if errorParsingDocIndex != nil {
-		return errorParsingDocIndex
+		return "", errorParsingDocIndex
 	}
-
-	var ymlstr = `
-notes:
- goal:
-   - to test using a simpler mode of data structure for tasks using array
- why:
-   - array will allow you to put more attribute so that you can put desc
-
-tasks:
- -
-   name: task
-   desc: this is task
-   task: #this comment will not be treated as desc of the task, removing # will invalid the yml
-     -
-       func: shell
-       desc: do step1 in shell func
-       do:
-         - echo "hello"
-         - echo "world"
-
-     -
-       func: shell
-       desc: do step2 in shell func
-       flags:
-         - ignore_error
-       do:
-         - echo "hello"
-         - echo "world"|grep non-exist
- -
-   name: task
-   desc: this is task
-   task: #this comment will not be treated as desc of the task, removing # will invalid the yml
-     -
-       func: shell
-       desc: do step1 in shell func
-       do:
-         - echo "hello"
-         - echo "world"
-
-     -
-       func: shell
-       desc: do step2 in shell func
-       flags:
-         - ignore_error
-       do:
-         - echo "hello"
-         - echo "world"|grep non-exist
-
-`
 
 	matchingNodes, errorReadingStream := cmReadYml(ymlstr, path, updateAll, docIndexInt)
 	if errorReadingStream != nil {
-		return errorReadingStream
+		return "", errorReadingStream
 	}
 	var buf *YmlResult
 	err := printResults(matchingNodes, buf)
 
 	log.Infof("read result: \n%s", readResult)
-	return err
+	return readResult, err
 }
 
 var (
--- cmd/upsvc_test.go
+++ cmd/upsvc_test.go
@@ -1,9 +1,96 @@
 package cmd
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestCmSvcReadProperty(t *testing.T) {
-	UpReadYml("vvvv")
+var (
+	ymlstr = `
+notes:
+ goal:
+   - to test using a simpler mode of data structure for tasks using array
+ why:
+   - array will allow you to put more attribute so that you can put desc
+
+tasks:
+ -
+   name: task1
+   desc: this is task1
+   task: #this comment will not be treated as desc of the task, removing # will invalid the yml
+     -
+       func: shell
+       desc: do step1 in shell func
+       do:
+         - echo "hello"
+         - echo "world"
+
+     -
+       func: cmd
+       desc: do step2 in shell func
+       flags:
+         - ignore_error
+       do:
+         - echo "hello"
+         - echo "world"|grep non-exist
+ -
+   name: task2
+   desc: this is task2
+   task: #this comment will not be treated as desc of the task, removing # will invalid the yml
+     -
+       func: shell
+       desc: do step1 in shell func
+       do:
+         - echo "hello"
+         - echo "world"
+
+     -
+       func: cmd
+       desc: do step2 in shell func
+       flags:
+         - ignore_error
+       do:
+         - echo "hello"
+         - echo "world"|grep non-exist
+
+`
+)
+
+func TestUpReadYml01(t *testing.T) {
+	var path = "tasks.**.task"
+	result, _ := UpReadYml(ymlstr, path, "vvv")
+	fmt.Println(result)
+}
+
+func TestUpReadYml02(t *testing.T) {
+	var path = "tasks.[1]"
+	result, _ := UpReadYml(ymlstr, path, "vvv")
+	fmt.Println(result)
+}
+
+func TestUpReadYml03(t *testing.T) {
+	var path = "tasks.[*]"
+	result, _ := UpReadYml(ymlstr, path, "vvv")
+	fmt.Println(result)
+}
+
+func TestUpReadYml04(t *testing.T) {
+	var path = "tasks(name==task2)"
+	result, _ := UpReadYml(ymlstr, path, "vvv")
+	fmt.Println(result)
+}
+
+func TestUpReadYml05(t *testing.T) {
+	var path = "tasks(name==task*)"
+	UpReadYml(ymlstr, path, "vvvv")
+}
+
+func TestUpReadYml06(t *testing.T) {
+	var path = "tasks(name==task*).desc"
+	UpReadYml(ymlstr, path, "vvvv")
+}
+
+func TestUpReadYml07(t *testing.T) {
+	var path = "tasks(name==task*).task(func==cmd)"
+	UpReadYml(ymlstr, path, "vvvv")
 }
